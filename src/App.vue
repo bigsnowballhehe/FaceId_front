1e459017a2087606fff6d4c4bf8b3f613de5f6e8
Your Name <you@example.com>
2020-11-07 17:02:16 +0800

    add test

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,32 @@
 <template>
-  <div id="app">
-    ·
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="下次一定好好，这是一个，传值，有prop，我就传给你喽？" />
-    <LearningTest>
-      <template v-slot:default="slotProps">
-        {{ slotProps.user.age }}
-      </template>
-    </LearningTest>
-  </div>
+    <div id="app">
+        <!-- <LearningTest love="what" heat="who">
+            <template v-slot:default="slotProps">
+                {{ slotProps.user.age }}
+            </template>
+        </LearningTest> -->
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import LearningTest from "./components/LearningTest";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-    LearningTest,
-  },
+    name: "App",
+    components: {},
+    data() {
+        return {};
+    },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    display: flex;
+    align-content: space-around;
 }
 </style>
--- src/components/LearningTest.vue
+++ src/components/LearningTest.vue
@@ -1,10 +1,15 @@
 <template>
-    <span>
-        年是
-        <slot v-bind:user="user">
-            {{ user.name }}
-        </slot>
-    </span>
+    <div :style="{ fontSize: postFontsize + 'em' }">
+        <button @click="postFontsize += 0.1">touch me +</button>
+        <H1>{{ love }}</H1>
+        <h2>{{ heat }}</h2>
+        <span>
+            年龄是
+            <slot v-bind:user="user">
+                {{ user.name }}
+            </slot>
+        </span>
+    </div>
 </template>
 
 <script>
@@ -13,12 +18,14 @@ export default {
 
     data() {
         return {
+            postFontsize: 1,
             user: {
                 name: "xueqiu",
                 age: "23",
             },
         };
     },
+    props: ["love", "heat"],
 };
 </script>
 
