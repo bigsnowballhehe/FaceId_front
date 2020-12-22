065a39439a8e3fb76457a1578b570b6588ebaf15
wei wu <976313853@qq.com>
2020-12-22 15:47:03 +0800

    改点首页

--- src/components/intro.vue
+++ src/components/intro.vue
@@ -6,6 +6,9 @@
         <div class="wecl">
             <!-- <img src="../assets/wecl.jpg" alt="欢迎" /> -->
            <h1>考勤记录系统</h1>
+           <div class="intro">
+               <span>本系统是基于百度人脸识别API的考勤记录系统，可以查看关于在注册人员的考勤记录，以及更直观的可视化界面</span>
+           </div>
         </div>
         
     </div>
@@ -19,15 +22,23 @@
     // text-align: center;
     border-top:1px solid gray;
     margin-top:10px;
+    display:flex;
+    flex-direction:column;
     
 }
-.wecl > h1{
+.wecl  h1{
     font-weight: 500;
     font-size:30px;
     line-height:38px;
     color:#0d1a26;
     margin-top:10px;
     margin-bottom: 20px;
+    align-self: center;
 }
-
+.app{
+    height: 600px;
+}
+.wecl .intro{
+    align-self: center;
+}
 </style>
